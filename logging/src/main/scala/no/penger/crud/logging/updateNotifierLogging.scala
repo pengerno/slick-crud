7b316fae4cbc6ccbc99f69c9ca79cd146ecde99e
oyviberg <oyvind.raddum.berg@finn.no>
2014-10-06 14:01:26 +0200

    these shouldnt have been final

--- logging/src/main/scala/no/penger/crud/logging/updateNotifierLogging.scala
+++ logging/src/main/scala/no/penger/crud/logging/updateNotifierLogging.scala
@@ -7,10 +7,10 @@ trait updateNotifierLogging extends updateNotifier {
   trait UpdateNotifierLogging extends UpdateNotifier {
     self: LazyLogging =>
 
-    override final def updated[ID, T](t: TableName, id: ID)(u: Update) =
+    override def updated[ID, T](t: TableName, id: ID)(u: Update) =
       logger.info(s"updated table $t for row $id for column ${u.column} from ${u.oldValue} to ${u.newValue}")
 
-     override final def updateFailed[ID](t: TableName, id: ID)(f: FailedUpdate) =
+     override def updateFailed[ID](t: TableName, id: ID)(f: FailedUpdate) =
       logger.warn(s"could not update table $t for row $id for column ${f.column} and value ${f.value}}", f.t)
   }
 }
