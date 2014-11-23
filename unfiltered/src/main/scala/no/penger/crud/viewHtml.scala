e18efb13d9420670ad88b103b34a28693f6dc821
Øyvind Raddum Berg <elacin@gmail.com>
2014-11-23 19:15:01 +0100

    Guarantee unique id for every table on a page, didn't think of this when i replaced the 'not found' message with a form to create the missing row

--- unfiltered/src/main/scala/no/penger/crud/viewHtml.scala
+++ unfiltered/src/main/scala/no/penger/crud/viewHtml.scala
@@ -23,46 +23,50 @@ trait viewHtml extends view with viewFormatHtml {
     namedCells: NamedCellRow[ROW]) extends View[ID, ROW] {
 
     /* generate a random id for the table we render, for frontend to distinguish multiple tables */
-    val uniqueId    = tableName+UUID.randomUUID().toString.filter(_.isLetterOrDigit)
+    def newUniqueId = tableName+UUID.randomUUID().toString.filter(_.isLetterOrDigit)
+    def withId[T](f: String ⇒ T) = f(newUniqueId)
 
     override def renderCell(columnName: ColumnName, value: Any, cell: Cell[Any]): ElemFormat =
       <td>{
-      if (idCol =:= columnName)
-        <a href={base + "/" + cell.toStr(value)} class="btn-style">{cell.toStr(value)}</a>
-      else if (cell.inputType == "checkbox")
-        enabled(isEditable && cell.isEditable)(checkedCheckbox(cell.inputType == "checkbox" && (value == true || value == Some(true)))(
-          <input type="checkbox"/>
-        ))
-      else
-        enabled(isEditable && cell.isEditable)(<input
-            class={if (cell.alignRight) "right" else "left"}
-            type={cell.inputType}
-            placeholder={cell.typeName}
-            value={cell.toStr(value)}
-            autocomplete="off"
-          />)
-        }
+        if (idCol =:= columnName)
+          <a href={base + "/" + cell.toStr(value)} class="btn-style">{cell.toStr(value)}</a>
+        else if (cell.inputType == "checkbox")
+          enabled(isEditable && cell.isEditable)(checkedCheckbox(cell.inputType == "checkbox" && (value == true || value == Some(true)))(
+            <input type="checkbox"/>
+          ))
+        else
+          enabled(isEditable && cell.isEditable)(<input
+              class={if (cell.alignRight) "right" else "left"}
+              type={cell.inputType}
+              placeholder={cell.typeName}
+              value={cell.toStr(value)}
+              autocomplete="off"
+            />)
+          }
       </td>
 
-    override def many(rows: Seq[(ID, ROW)]) =
-      <div>
-        <table id={uniqueId}>
-          {header}
-          <thead>
-            <tr>{namedCells.colNames.map(name ⇒ <th class="columnHeader">{name}</th>)} </tr>
-          </thead><tbody>{
-            rows.zipWithIndex.map {
-              case ((id, row), idx) ⇒ <tr db-id={Cell.toStr(id)} class={if (idx % 2 == 0) "even" else ""}>{
-                namedCells.cellsWithUnpackedValues(row).map {
-                  case ((colName, cell), value) ⇒ renderCell(colName, value, cell)
-                }
-              }</tr>
-            }
-          }</tbody></table>
-        <script type="text/javascript">no.penger.crud.view('{base}', '#{uniqueId}')</script>
-      </div>
+    override def many(rows: Seq[(ID, ROW)]) = withId{
+      uniqueId ⇒
+        <div>
+          <table id={uniqueId}>
+            {header}
+            <thead>
+              <tr>{namedCells.colNames.map(name ⇒ <th class="columnHeader">{name}</th>)} </tr>
+            </thead><tbody>{
+              rows.zipWithIndex.map {
+                case ((id, row), idx) ⇒ <tr db-id={Cell.toStr(id)} class={if (idx % 2 == 0) "even" else ""}>{
+                  namedCells.cellsWithUnpackedValues(row).map {
+                    case ((colName, cell), value) ⇒ renderCell(colName, value, cell)
+                  }
+                }</tr>
+              }
+            }</tbody></table>
+          <script type="text/javascript">no.penger.crud.view('{base}', '#{uniqueId}')</script>
+        </div>
+    }
 
-    override def single(id: ID, row: ROW) = {
+    override def single(id: ID, row: ROW) = withId {
+      uniqueId ⇒
         <table id={uniqueId} db-id={Cell.toStr(id)}>
           {header}
           <thead><tr><th>Column</th><th>Value</th></tr></thead>
@@ -73,24 +77,25 @@ trait viewHtml extends view with viewFormatHtml {
         <script type="text/javascript">{s"no.penger.crud.single('$base', '#$uniqueId')"}</script>
     }
 
-    override def newPage(errorOpt: Option[String]) = {
-      <table id={uniqueId}>
-        <caption class="columnHeader">
-          <strong>{
-            errorOpt match {
-              case Some(error) => error + ". create it here"
-              case _           => tableName
-            }}</strong>
-          <a             class="btn-style" href={base} >See all</a>
-          <a id="submit" class="btn-style" href="#"    >Save</a>
-        </caption>
-        <tbody> {
-          namedCells.cells.map{
-            case (name, cell) ⇒ <tr><th class="columnHeader">{name}</th>{renderEmptyCell(cell)}</tr>
-          }
-        }</tbody>
-      </table>
-      <script type="text/javascript">{s"no.penger.crud.neew('$base', '#$uniqueId')"}</script>
+    override def newPage(errorOpt: Option[String]) = withId {
+      uniqueId ⇒
+        <table id={uniqueId}>
+          <caption class="columnHeader">
+            <strong>{
+              errorOpt match {
+                case Some(error) => error + ". create it here"
+                case _           => tableName
+              }}</strong>
+            <a             class="btn-style" href={base} >See all</a>
+            <a id="submit" class="btn-style" href="#"    >Save</a>
+          </caption>
+          <tbody> {
+            namedCells.cells.map{
+              case (name, cell) ⇒ <tr><th class="columnHeader">{name}</th>{renderEmptyCell(cell)}</tr>
+            }
+          }</tbody>
+        </table>
+        <script type="text/javascript">{s"no.penger.crud.neew('$base', '#$uniqueId')"}</script>
     }
 
     def renderEmptyCell(cell: Cell[Any]): ElemFormat =
